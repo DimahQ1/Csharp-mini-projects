b8ece889360aa77c8f182da428b4d3ba26fb59dd
DimahQ1 <hammyghareghani@gmail.com>
2025-03-03 16:30:44 +0330

    Adding Calculator

---
+++ ConsoleAppC#/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppC_
+{
+    class Calculator
+    {
+        // Define Calculator Method And Getting Numbers For Calculating
+        public double Calculate(double n1, double n2 , char op)
+        {
+            switch (op)
+            {
+                case '+': return n1 + n2;
+                case '-': return n1 - n2;
+                case '*': return n1 * n2;
+                case '/':
+                    // Exception Handling For DivisionByZero Error
+                    if (n2 == 0)
+                    {
+                        throw new DivideByZeroException("Error: Division by zero is not allowed.");
+                    }
+                    return n1 / n2;
+                // Exception Handling For Any Exception:)
+                default: throw new ArgumentException("Invalid operator try these : *, /, -, +");
+            }
+        }
+    }
+}
---
+++ ConsoleAppC#/Program.cs
@@ -0,0 +1,66 @@
+﻿using ConsoleAppC_;
+using System;
+using System.Net.Http.Headers;
+// This Program for manage calculator class
+class Program
+{
+    // Method For Check Number IsValid or NOT!
+    static double GetValidNumber(string prompt)
+    {
+        double number;
+        while (true)
+        {
+            Console.WriteLine(prompt);
+            if(double.TryParse(Console.ReadLine(), out number))
+            {
+                return number;
+            }
+            Console.WriteLine("Error: Invalid input, Please Enter A Valid Number: ");
+        }
+    }
+    // Method For Check Operator Is Valid Or NOT
+    static char GetValidOperator()
+    {
+        char op;
+        while (true)
+        {
+            Console.WriteLine("Enter Operator ( * , / , - , + )");
+            if(char.TryParse(Console.ReadLine(), out op) && "+/-*".Contains(op))
+            {
+                return op;
+            }
+            Console.WriteLine("Error: Invalid Operator! Please Enter A Valid Operator");
+        }
+    }
+    static void Main()
+    {
+        // Define Calculator Class
+        Calculator calculator = new Calculator();
+
+        // Define A Loop For Unlimited Calculating :)
+        while (true)
+        {
+            try
+            {
+                // Variables && Calls
+                double n1 = GetValidNumber("Enter first number: ");
+                char sign = GetValidOperator();
+                double n2 = GetValidNumber("Enter second number: ");
+                double result = calculator.Calculate(n1, n2, sign);
+
+                // Result Section
+                Console.WriteLine($"{n1} {sign} {n2} = {result}");
+            }
+            catch(Exception e)
+            {
+                // Simple Exception Message
+                Console.WriteLine("Error: " + e.Message);
+            }
+            Console.WriteLine("Do you want another calculation? y or n");
+            if(Console.ReadLine().ToLower() != "y")
+            {
+                break;
+            }
+        }
+    }
+}
